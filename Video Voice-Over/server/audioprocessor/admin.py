61933b449dd22fc2261a834fe7771cb54aac055e
hemaha194 <hemaha194@gmail.com>
2025-02-10 19:40:27 +0530

    deleting voice over

--- Video Voice-Over/server/audioprocessor/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Video,BgMusic,combinedMusic,ExampleTable
-
-# Register your models here.
-admin.site.register(Video)
-admin.site.register(BgMusic)
-admin.site.register(combinedMusic)
-admin.site.register(ExampleTable)
--- Video Voice-Over/server/audioprocessor/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AudioprocessorConfig(AppConfig):
-    name = 'audioprocessor'
