53b668a39015c715a42273be09e0f1253be5781c
Hema <127856773+hemaha194@users.noreply.github.com>
2025-02-11 22:42:52 +0530

    Add files via upload

---
+++ Mind Map Project/client/src/App.js
@@ -0,0 +1,12 @@
+// import Home from "./components/Home";
+import Home from "./components/Routes";
+// import Home from "./components/example";
+function App() {
+  return (
+    <div>
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
