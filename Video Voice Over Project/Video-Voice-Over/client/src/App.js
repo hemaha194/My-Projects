f306194725e57486403fc49892dda545ed1567c0
Hema <127856773+hemaha194@users.noreply.github.com>
2025-02-11 23:01:06 +0530

    Add files via upload

---
+++ Video Voice Over Project/Video-Voice-Over/client/src/App.js
@@ -0,0 +1,13 @@
+// import Home from "./components/AudioProcessor";
+import Home from "./components/Layout"; //this is for reactflow ffmpeg 
+// import Home from "./components/example";
+// import Home from "./Traverse/Home"
+function App() {
+  return (
+    <div className="App">
+    <Home/>
+    </div>
+  );
+}
+
+export default App;
