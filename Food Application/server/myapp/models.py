c185ea05431f650413c0158499b85dc4e12e289c
hema23194 <hema.pantham@accelyzei.com>
2025-08-13 12:07:03 +0530

    Adding Razorpay feature to the food items

--- Food Application/server/myapp/models.py
+++ Food Application/server/myapp/models.py
@@ -8,6 +8,18 @@ class RecipeTable(models.Model):
     r_type=models.CharField(max_length=20)
     r_created_at=models.DateField()
     r_img=models.CharField(max_length=200)
+    r_price=models.CharField(max_length=20,blank=True, null=True)
 
     def __str__(self):
         return self.r_name
+    
+class Orders(models.Model):
+    name = models.CharField(max_length=100)
+    recipe = models.ForeignKey(RecipeTable,on_delete=models.CASCADE, related_name='orders',null=True, blank=True)
+    amount = models.CharField(max_length=100)
+    order_id = models.CharField(max_length=100,blank=True, null=True)
+    razorpay_payment_id = models.CharField(max_length=100,blank=True, null=True)
+    paid = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
